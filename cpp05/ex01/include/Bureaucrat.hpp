f94e0ef24ce00a66501d7b06139ee4a92a59f7da
Kevin Hirsig <khirsig@1-B-8.42heilbronn.de>
2022-06-09 10:10:44 +0200

    cpp05/ex01 added, and finished

---
+++ cpp05/ex01/include/Bureaucrat.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khirsig <khirsig@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 12:14:48 by khirsig           #+#    #+#             */
+/*   Updated: 2022/06/09 10:02:19 by khirsig          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+# include "Form.hpp"
+
+class Form;
+
+class Bureaucrat {
+	class GradeTooHighException : public std::exception {
+		virtual const char *what() const throw();
+	};
+	class GradeTooLowException : public std::exception {
+		virtual const char *what() const throw();
+	};
+
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string name);
+		Bureaucrat(const std::string name, const int grade);
+		Bureaucrat(const Bureaucrat &other);
+		~Bureaucrat();
+
+		Bureaucrat	&operator=(const Bureaucrat &other);
+
+		std::string	getName() const;
+		int			getGrade() const;
+		void		incrementGrade();
+		void		decrementGrade();
+		void		signForm(Form &f);
+	private:
+		const std::string	_name;
+		int					_grade;
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &output);
+
+#endif
---
+++ cpp05/ex01/include/Form.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khirsig <khirsig@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 15:30:13 by khirsig           #+#    #+#             */
+/*   Updated: 2022/06/09 10:05:11 by khirsig          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include <string>
+# include <iostream>
+# include <exception>
+# include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class Form {
+	class GradeTooHighException : public std::exception {
+		virtual const char *what() const throw();
+	};
+	class GradeTooLowException : public std::exception {
+		virtual const char *what() const throw();
+	};
+
+	public:
+		Form();
+		Form(const std::string name, const int gradeSign, const int gradeExecute);
+		Form(const Form &other);
+		~Form();
+
+		Form	&operator=(const Form &other);
+
+		std::string getName() const;
+		bool		getIsSigned() const;
+		int			getGradeSign() const;
+		int			getGradeExecute() const;
+
+		void		beSigned(const Bureaucrat &b);
+	private:
+		const std::string	_name;
+		bool				_isSigned;
+		const int			_gradeSign;
+		const int			_gradeExecute;
+};
+
+std::ostream &operator<<(std::ostream &os, const Form &output);
+
+#endif
